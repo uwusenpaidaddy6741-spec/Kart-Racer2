1ca036541f1baf38caa39e8ad23188352e88f245
uwusenpaidaddy6741-spec <uwusenpaidaddy6741@gmail.com>
2026-09-20 00:53:57 -0400

    Create wheels.js

---
+++ data/data/wheels.js
@@ -0,0 +1,86 @@
+export const wheels = {
+
+    street: {
+
+        name: "Street",
+
+        speed: 0,
+
+        acceleration: 0,
+
+        handling: 0,
+
+        drift: 1.0
+    },
+
+
+    grip: {
+
+        name: "Grip",
+
+        speed: -2,
+
+        acceleration: 0,
+
+        handling: 0.5,
+
+        drift: 1.1
+    },
+
+
+    speed: {
+
+        name: "Speed",
+
+        speed: 4,
+
+        acceleration: -1,
+
+        handling: -0.2,
+
+        drift: 0.9
+    },
+
+
+    drift: {
+
+        name: "Drift",
+
+        speed: -1,
+
+        acceleration: 0,
+
+        handling: 0.4,
+
+        drift: 1.5
+    },
+
+
+    cyclone: {
+
+        name: "Cyclone",
+
+        speed: 0,
+
+        acceleration: 2,
+
+        handling: 1.2,
+
+        drift: 1.5
+    },
+
+
+    shadow: {
+
+        name: "Shadow",
+
+        speed: 2,
+
+        acceleration: 1,
+
+        handling: 0.7,
+
+        drift: 1.4
+    }
+
+};
