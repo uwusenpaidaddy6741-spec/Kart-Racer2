e9a0348a4240707df4edc95790f88847884ba563
uwusenpaidaddy6741-spec <uwusenpaidaddy6741@gmail.com>
2026-09-20 00:54:30 -0400

    Create gliders.js

---
+++ data/gliders.js
@@ -0,0 +1,50 @@
+export const gliders = {
+
+    starter: {
+
+        name: "Starter",
+
+        glideSpeed: 1.0,
+
+        airControl: 1.0,
+
+        acceleration: 0
+    },
+
+
+    feather: {
+
+        name: "Feather",
+
+        glideSpeed: 1.1,
+
+        airControl: 1.15,
+
+        acceleration: 0
+    },
+
+
+    rocket: {
+
+        name: "Rocket",
+
+        glideSpeed: 1.2,
+
+        airControl: 0.9,
+
+        acceleration: 0.05
+    },
+
+
+    parachute: {
+
+        name: "Parachute",
+
+        glideSpeed: 0.95,
+
+        airControl: 1.3,
+
+        acceleration: 0
+    }
+
+};
