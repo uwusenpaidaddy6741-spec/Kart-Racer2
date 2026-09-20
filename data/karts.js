6e64ae0bf57fd77deb09a07cc316497b581fa459
uwusenpaidaddy6741-spec <uwusenpaidaddy6741@gmail.com>
2026-09-20 00:53:21 -0400

    Create karts.js

---
+++ data/karts.js
@@ -0,0 +1,66 @@
+export const karts = {
+
+    starter: {
+
+        name: "Starter",
+
+        speed: 40,
+
+        acceleration: 18,
+
+        braking: 25,
+
+        handling: 2.7,
+
+        weight: 1.0
+    },
+
+
+    speedster: {
+
+        name: "Speedster",
+
+        speed: 46,
+
+        acceleration: 15,
+
+        braking: 23,
+
+        handling: 2.4,
+
+        weight: 0.9
+    },
+
+
+    rocket: {
+
+        name: "Rocket",
+
+        speed: 42,
+
+        acceleration: 25,
+
+        braking: 22,
+
+        handling: 2.5,
+
+        weight: 0.8
+    },
+
+
+    heavy: {
+
+        name: "Heavy",
+
+        speed: 44,
+
+        acceleration: 12,
+
+        braking: 32,
+
+        handling: 2.2,
+
+        weight: 1.4
+    }
+
+};
